742de93cb24ba317f5b8108c844f4255d28a4215
w4alf <cbravo2004@yahoo.com>
2019-11-21 10:14:32 -0500

    new commit

---
+++ js/hello-world.js
@@ -0,0 +1,4 @@
+"use strict";
+var hw = "Hello world";
+var nameStudent = "student";
+console.log(hw, nameStudent);
---
+++ ts/hello-world.ts
@@ -0,0 +1,3 @@
+let hw:string = "Hello world";
+let nameStudent:string = "student";
+console.log(hw,nameStudent);
